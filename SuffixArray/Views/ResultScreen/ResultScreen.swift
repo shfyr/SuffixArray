4dd39a7dc5c6f0468f3f502321e54ea4117c1686
shfyr <lizavetaprokudina@gmail.com>
2025-01-01 18:36:33 +0300

    Search UI Added

--- SuffixArray/Views/ResultScreen/ResultScreen.swift
+++ SuffixArray/Views/ResultScreen/ResultScreen.swift
@@ -10,57 +10,75 @@ import SwiftUI
 struct ResultScreen: View {
     @StateObject var presenter: ResultScreenPresenter
     @State private var selectedOption = 0
+    @State private var searchText = ""
     
     var body: some View {
-        VStack {
-            Text("Text: \(presenter.text)")
-                .font(.title)
-                .padding()
-            
-            Picker("Options", selection: $selectedOption) {
-                Text("Ascending").tag(0)
-                Text("Descending").tag(1)
-                Text("Top 10").tag(2)
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            List {
-                switch selectedOption {
-                case 0:
-                    ForEach(presenter.allSuffixes.sorted(), id: \.self) { suffix in
-                        HStack {
-                            Text(suffix)
-                                .multilineTextAlignment(.leading)
-                            Spacer()
-                            Text(String(presenter.allSuffixes.frequency[suffix] ?? 1))
-                        }
-                    }
-                case 1:
-                    ForEach(presenter.allSuffixes.sorted(by: >), id: \.self) { suffix in
-                        HStack {
-                            Text(suffix)
-                                .multilineTextAlignment(.leading)
-                            Spacer()
-                            Text(String(presenter.allSuffixes.frequency[suffix] ?? 1))
-                        }
-                    }
-                case 2:
-                    ForEach(
-                        presenter.getTopTen(),
-                        id: \.0) { suffix in
-                            HStack {
-                                Text(suffix.0)
-                                    .multilineTextAlignment(.leading)
-                                Spacer()
-                                Text("\(suffix.1)")
-                            }
-                        }
-                default:
-                    Text("Invalid option")
+  
+            VStack {
+                ScrollView {
+                    Text("Text: \(presenter.text)")
+                        .font(.title)
+                        .padding()
+                        .lineLimit(nil)
+                        .multilineTextAlignment(.leading)
+//                        .background(.mint)
                 }
+                .frame(height: 250)
                 
+                Picker("Options", selection: $selectedOption) {
+                    Text("Ascending").tag(0)
+                    Text("Descending").tag(1)
+                    Text("Top 10").tag(2)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                
+                TextField("Search Suffixes", text: $searchText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .font(.largeTitle)
+                    .padding([.leading, .trailing])
+                List {
+                    select(option: selectedOption)
+                }
             }
+        .navigationTitle("Result")
+    }
+    
+    @ViewBuilder
+    func select(option: Int) -> some View {
+        switch option {
+        case 0:
+        ForEach(presenter.allSuffixes.sorted(), id: \.self) { suffix in
+                HStack {
+                    Text(suffix)
+                        .multilineTextAlignment(.leading)
+                    Spacer()
+                    Text(String(presenter.allSuffixes.frequency[suffix] ?? 1))
+                }
+            }
+        case 1:
+            ForEach(presenter.allSuffixes.sorted(by: >), id: \.self) { suffix in
+                HStack {
+                    Text(suffix)
+                        .multilineTextAlignment(.leading)
+                    Spacer()
+                    Text(String(presenter.allSuffixes.frequency[suffix] ?? 1))
+                }
+            }
+        case 2:
+            ForEach(
+                presenter.getTopTen(),
+                id: \.0) { suffix in
+                    HStack {
+                        Text(suffix.0)
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                        Text("\(suffix.1)")
+                    }
+                }
+        default:
+            Text("Invalid option")
         }
-        .navigationTitle("Result")    }
+    }
 }
 
 #Preview {
